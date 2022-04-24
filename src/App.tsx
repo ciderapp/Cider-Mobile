b44fb306d2d8e809763587b0317e0b0e7612c33d
GamingLiamStudios <58615717+GamingLiamStudios@users.noreply.github.com>
2022-04-25 02:17:48 +1000

    Dev Token Leaked?!

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text} from 'react-native';
 
+const FetchDevToken = async () => {
+  const json = await fetch('https://api.cider.sh/v1', {
+    headers: {
+      'user-agent': 'Cider/0.0.1',
+    },
+  })
+    .then(res => {
+      return res.json();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  return await json.token;
+};
+
 const App = () => {
-  return <Text>Hello World!</Text>;
+  const [devToken, setDevToken] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      setDevToken(await FetchDevToken());
+    })();
+  }, []);
+
+  return (
+    <Text>
+      {devToken !== '' ? (
+        <Text>Hello World! you cock suckers. token {devToken}</Text>
+      ) : (
+        <Text>Hello World! you cock suckers.</Text>
+      )}
+    </Text>
+  );
 };
 
 export default App;
