04d71b96f5f310293fb261c1667ee46cef2302d4
GamingLiamStudios <58615717+GamingLiamStudios@users.noreply.github.com>
2022-04-27 14:39:54 +1000

    Fetching and Storage of User Token implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {Text} from 'react-native';
+import {Text, NativeModules} from 'react-native';
+import EncryptedStorage from 'react-native-encrypted-storage';
+const {AMTokenProvider} = NativeModules;
 
-const FetchDevToken = async () => {
-  const json = await fetch('https://api.cider.sh/v1', {
-    headers: {
-      'user-agent': 'Cider/0.0.1',
-    },
+const fetchJson = async (url: string, headers?: any) => {
+  return await fetch(url, {
+    headers: headers,
   })
     .then(res => {
       return res.json();
@@ -13,22 +13,70 @@ const FetchDevToken = async () => {
     .catch(error => {
       console.log(error);
     });
-  return await json.token;
 };
 
 const App = () => {
   const [devToken, setDevToken] = useState('');
+  const [usrToken, setUsrToken] = useState('');
 
   useEffect(() => {
     (async () => {
-      setDevToken(await FetchDevToken());
+      try {
+        // Fetch devToken
+        const fetched_devToken = await fetchJson('https://api.cider.sh/v1', {
+          'user-agent': 'Cider/0.0.1',
+        }).then(res => {
+          return res.token;
+        });
+        setDevToken(fetched_devToken);
+        console.log(fetched_devToken);
+
+        // Fetch usrToken
+        let fetched_usrToken = '';
+        const stored_usrToken = await EncryptedStorage.getItem('usrToken');
+        if (stored_usrToken === null) {
+          console.log('No usrToken found. Fetching...');
+          // Authorize user
+          fetched_usrToken = await AMTokenProvider.generateUserToken(
+            fetched_devToken,
+          );
+          EncryptedStorage.setItem('usrToken', fetched_usrToken);
+        } else {
+          // Verify usability of usrToken
+          const library_songs = await fetchJson(
+            'https://api.music.apple.com/v1/me/library/songs',
+            {
+              Authorization: 'Bearer ' + fetched_devToken,
+              'Music-User-Token': stored_usrToken,
+            },
+          );
+          if (library_songs.errors !== undefined) {
+            console.log('usrToken is invalid. Fetching...');
+            // Authorize user
+            fetched_usrToken = await AMTokenProvider.generateUserToken(
+              fetched_devToken,
+            );
+            EncryptedStorage.setItem('usrToken', fetched_usrToken);
+          }
+
+          fetched_usrToken = stored_usrToken;
+        }
+
+        setUsrToken(fetched_usrToken);
+        console.log(fetched_usrToken);
+      } catch (error) {
+        console.log(error);
+      }
     })();
   }, []);
 
   return (
     <Text>
       {devToken !== '' ? (
-        <Text>Hello World! you cock suckers. token {devToken}</Text>
+        <Text>
+          Hello World! you cock suckers. token {devToken} ===== usrToken
+          {usrToken}
+        </Text>
       ) : (
         <Text>Hello World! you cock suckers.</Text>
       )}
